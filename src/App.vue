43beb008d6a6d187b1a4e2a03ec03f3f1a231528
Islan Victhor <islan12victhor@gmail.com>
2023-04-23 20:22:54 -0300

    First draft

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+  import { ref, computed } from 'vue'
+  
+  //const state = reactive({ todos:  })
+  //const todos = ref(['Mimir', 'Jogar LoL', 'Estudar'])
+  const todos = ref([
+    {id: 0, string: 'Mimir', finished: false},
+    {id: 1, string: 'Jogar LoL', finished: false},
+    {id: 2, string: 'Estudar', finished: false}
+  ])
+  const filteredTodos = computed(() =>{
+    const filtered = []
+    return filtered
+  })
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <form>
+    <input>
+    <input type="button" value="Inserir">
+  </form>
+  <ul>
+    <li>{{todos[1].string}}</li>
+    <li>B</li>
+    <li>C</li>
+  </ul>
 </template>
