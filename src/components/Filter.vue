db967ab86a6ecacb2464dd88c0949127b183828e
Islan V <islan12victhor@gmail.com>
2023-04-28 17:33:26 -0300

    random

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,16 +1,19 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, watch } from 'vue'
   
   const checked = ref(null)
-  const props = defineProps(['filter'])
+  //const props = defineProps(['filter'])
   const emit = defineEmits(['change-filter'])
+  watch(checked, (newChecked) => {
+    emit('change-filter')
+  })
   /*const labels = [
     {id: 'All', isChecked:}
   ]*/
 </script>
 
 <template>
-  <input type="radio" id="All" value="All" v-model="checked" checked="">
+  <input type="radio" id="All" value="All" v-model="checked" checked>
   <label for = "All">All</label>
   
   <input type="radio" id="Open" value="Open" v-model="checked">
